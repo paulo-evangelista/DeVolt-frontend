45e9f6e9622b77471a40c96dedb3f1468ea56fd9
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-03-26 15:48:50 -0300

    fisrt

---
+++ packages/nextjs/app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GetUserGeolocationDialog from "~~/components/GetUserGeolocationDialog";
+import HomeHeroSection from "~~/components/HomeHeroSection";
+import MapSectionHomepage from "~~/components/MapSectionHomepage";
+import { LocationProvider, useLocation } from "~~/contexts/LocationContext";
+
+const Home: NextPage = () => {
+  return (
+    <LocationProvider>
+      <ToastContainer stacked theme="colored" position="top-center" />
+      <GetUserGeolocationDialog />
+      <div className="my-16 w-full flex flex-col md:grid-cols-2 gap-4">
+        <div className="mx-auto md:w-10/12 w-11/12">
+          <HomeHeroSection />
+          <div className="md:col-span-5 md:row-span-1 md:col-start-1 md:row-start-3 mt-6">
+            <MapSectionHomepage />
+          </div>
+        </div>
+      </div>
+    </LocationProvider>
+  );
+};
+
+export default Home;
